32dfaf193e16fdd09e86655fab516f7a67ffce41
vivycahyani <vivy.cahyani@selarastech.com>
2023-08-29 23:47:33 +0700

    [ADD] subcourse 5

---
+++ 5-underfitting-and-overfitting.py
@@ -0,0 +1,16 @@
+#python3 /Users/vivycahyani/Desktop/Belajar/kaggle-certification/kaggle-intro-to-machine-learning/5-underfitting-and-overfitting.py
+
+from sklearn.metrics import mean_absolute_error
+from sklearn.tree import DecisionTreeRegressor
+
+def get_mae(max_leaf_nodes, train_X, val_X, train_y, val_y):
+    model = DecisionTreeRegressor(max_leaf_nodes=max_leaf_nodes, random_state=0)
+    model.fit(train_X, train_y)
+    preds_val = model.predict(val_X)
+    mae = mean_absolute_error(val_y, preds_val)
+    return(mae)
+
+# compare MAE with differing values of max_leaf_nodes
+for max_leaf_nodes in [5, 50, 500, 5000]:
+    my_mae = get_mae(max_leaf_nodes, train_X, val_X, train_y, val_y)
+    print("Max leaf nodes: %d  \t\t Mean Absolute Error:  %d" %(max_leaf_nodes, my_mae))
