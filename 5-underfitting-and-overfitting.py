0196d9a8d2cbd5979040999f7dc094a6c09816af
“vivy-c” <cahyanivivy@gmail.com>
2023-09-18 18:54:15 +0700

    [IMP] 5th tutorial

--- 5-underfitting-and-overfitting.py
+++ 5-underfitting-and-overfitting.py
@@ -51,3 +51,13 @@ from learntools.core import binder
 binder.bind(globals())
 from learntools.machine_learning.ex5 import *
 print("\nSetup complete")
+
+from sklearn.metrics import mean_absolute_error
+from sklearn.tree import DecisionTreeRegressor
+
+def get_mae(max_leaf_nodes, train_X, val_X, train_y, val_y):
+    model = DecisionTreeRegressor(max_leaf_nodes=max_leaf_nodes, random_state=0)
+    model.fit(train_X, train_y)
+    preds_val = model.predict(val_X)
+    mae = mean_absolute_error(val_y, preds_val)
+    return(mae)
