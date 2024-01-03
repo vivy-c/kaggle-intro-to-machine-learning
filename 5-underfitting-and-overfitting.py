fbb20388cea9eb926f8e1743cb6081c628076fa2
vivy-c <cahyanivivy@gmail.com>
2024-01-03 23:45:31 +0700

    [IMP] model using all data

--- 5-underfitting-and-overfitting.py
+++ 5-underfitting-and-overfitting.py
@@ -63,4 +63,17 @@ def get_mae(max_leaf_nodes, train_X, val_X, train_y, val_y):
     return(mae)
         
     #test
-   
+
+    candidate_max_leaf_nodes = [5, 25, 50, 100, 250, 500]
+    # Write loop to find the ideal tree size from candidate_max_leaf_nodes
+    _
+
+    # Store the best value of max_leaf_nodes (it will be either 5, 25, 50, 100, 250 or 500)
+    score = {leaf_size : get_mae(leaf_size, train_X, val_X, train_y, val_y) for leaf_size in candidate_max_leaf_nodes}
+    best_tree_size = min(score, key=score.get)
+
+    # Fill in argument to make optimal size and uncomment
+    final_model = DecisionTreeRegressor(max_leaf_nodes = best_tree_size, random_state=1)
+
+    # fit the final model and uncomment the next two lines
+    final_model.fi t(X, y)
